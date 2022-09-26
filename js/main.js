218ea42548a2568cc808f946b7927a5fe57d7c24
Nkaltaeva <n.kaltaeva@ya.ru>
2022-09-26 15:29:13 +0300

    prof_js_2

---
+++ js/main.js
@@ -0,0 +1,89 @@
+class ProductList{
+    constructor(container='.products'){
+        this.container = container;
+        this.goods = [];
+        this.allProducts = [];//рекомендация, чтобы метод был вызван в текущем классе
+        this._fetchProducts();//вывод товаров на страницу
+    }
+    _fetchProducts(){
+        this.goods = [
+            {id: 1, title: 'Notebook', price: 2000},
+            {id: 2, title: 'Mouse', price: 20},
+            {id: 3, title: 'Keyboard', price: 200},
+            {id: 4, title: 'Gamepad', price: 50},
+        ];
+    }    
+    render(){
+        const block = document.querySelector(this.container);
+        for(let product of this.goods){
+            const productObj = new ProductItem(product);
+            this.allProducts.push(productObj);
+            block.insertAdjacentHTML('beforeend',productObj.render())
+             // block.innerHTML += item.render();
+        }
+    }
+    getSum() {
+        let res = this.allProducts.reduce((s, item) => s + item.price,0);
+        alert(res);
+    }
+}
+
+class ProductItem{
+    constructor(product,img='https://via.placeholder.com/200x150'){
+        this.title = product.title;
+        this.id = product.id;
+        this.price = product.price;
+        this.img = img;
+    }
+    render(){
+           return `<div class="product-item">
+                <img src="${this.img}">
+                <h3>${this.title}</h3>
+                <p>${this.price}</p>
+                <button class="buy-btn">Купить</button>
+            </div>`
+    }
+}
+
+let list = new ProductList();
+list.render();
+list.getSum();
+class Basket {
+    addGood() {
+
+    }
+    removeGood() {
+
+    }
+    changeGood() {
+
+    }
+    
+    render(){
+        
+    }
+}
+class ElemBasket {
+    render(){}
+}
+
+
+
+//const products = [
+//    {id: 1, title: 'Notebook', price: 2000},
+//    {id: 2, title: 'Mouse', price: 20},
+//    {id: 3, title: 'Keyboard', price: 200},
+//    {id: 4, title: 'Gamepad', price: 50},
+//];
+//
+//const renderProduct = (product,img='https://placehold.it/200x150') => {
+//    return `<div class="product-item">
+//                <img src="${img}">
+//                <h3>${product.title}</h3>
+//                <p>${product.price}</p>
+//                <button class="buy-btn">Купить</button>
+//            </div>`
+//};
+//const renderPage = list => document.querySelector('.products').innerHTML = list.map(item => renderProduct(item)).join('');
+//
+//renderPage(products);
